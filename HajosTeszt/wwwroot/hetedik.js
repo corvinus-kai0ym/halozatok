4de42fd17175f3b9f7d98119ca3c4e569e73215c
Áron László Makrai <aronmakrai@gmail.com>
2021-03-28 18:05:30 +0200

    kérdések 7.hét kész

--- HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/wwwroot/hetedik.js
@@ -1,17 +1,5 @@
 var kérdések;
-var jelenlegiKérdés=0;
-
-let válasz1=document.getElementById("válasz1");
-let válasz2=document.getElementById("válasz2");
-let válasz3=document.getElementById("válasz3");
-
-valasz1.addEventListener('click', () => { választVálasz(1, válasz1) });
-valasz2.addEventListener('click', () => { választVálasz(2, válasz2) });
-valasz3.addEventListener('click', () => { választVálasz(3, válasz3) });
-
-
-elore.addEventListener('click', () => { kérdésVáltó(1) });
-vissza.addEventListener('click', () => { kérdésVáltó(-1) });
+var jelenlegiKérdés = 0;
 
 function letoltes(){
 
@@ -19,6 +7,7 @@ function letoltes(){
     .then(Response => Response.json())
     .then(adat=> adatÉrkezett(adat));
 
+    console.log("Leltöltés kész!")
 }
 
 function adatÉrkezett(adat){
@@ -32,26 +21,46 @@ function letöltésBefejeződött(d){
 
     console.log('Sikeres letöltés!');
     console.log(d);
-    kérdések=d;
+    kérdések = d;
+
+    kérdésMegjelenítés(jelenlegiKérdés);
+    console.log("jelenlegi kérdés megjelenítve")
+
+    let előre = document.getElementById("előre");
+    let vissza = document.getElementById("vissza");
+
+    let válasz1 = document.getElementById("válasz1");
+    let válasz2 = document.getElementById("válasz2");
+    let válasz3 = document.getElementById("válasz3");
+
+    előre.addEventListener('click', () => { kérdésVáltó(1) });
+    vissza.addEventListener('click', () => { kérdésVáltó(-1) });
+
+    válasz1.addEventListener('click', () => { választVálasz(1, válasz1) });
+    válasz2.addEventListener('click', () => { választVálasz(2, válasz2) });
+    válasz3.addEventListener('click', () => { választVálasz(3, válasz3) });
+
 }
 
-window.onload(letoltes(),
-kérdésMegjelenítés());
+window.onload = letoltes();
 
-function kérdésMegjelenítés(i){
+function kérdésMegjelenítés(i) {
+
+    console.log(i);
 
     let kérdés_szöveg = document.getElementById("kérdés_szöveg");
     let kép= document.getElementById("kép1");
-    
 
-    kérdés_szöveg.innerText=kérdések[i].questionText;
+    
+    kérdés_szöveg.innerHTML=kérdések[i].questionText;
     válasz1.innerHTML=kérdések[i].answer1;
     válasz2.innerHTML=kérdések[i].answer2;
     válasz3.innerHTML=kérdések[i].answer3;
-    
+
+ 
     if(kérdések[i].image !="")
     {
-        kép.src="https://szoft1.comeback.hu/hajo/"+kerdesek[i].image
+        kép.src="https://szoft1.comeback.hu/hajo/"+kérdések[i].image
         kép.hidden=false;
     }
     else
@@ -63,27 +72,31 @@ function kérdésMegjelenítés(i){
 
 function kérdésVáltó(irány) {
     if (irány > 0) {
-        if (jelenlegiKerdes + 1 > kerdesek.length - 1) {
-            jelenlegiKerdes = 0;
+        if (jelenlegiKérdés + 1 > kérdések.length - 1) {
+            jelenlegiKérdés = 0;
         }
         else {
-            jelenlegiKerdes++;
+            jelenlegiKérdés++;
         }
     }
     else {
-        if (jelenlegiKerdes - 1 == -1) {
-            jelenlegiKerdes = kerdesek.length - 1;
+        if (jelenlegiKérdés - 1 == -1) {
+            jelenlegiKérdés = kérdések.length - 1;
         }
         else {
-            jelenlegiKerdes--;
+            jelenlegiKérdés--;
         }
     }
+    
+    kérdésMegjelenítés(jelenlegiKérdés);
+    
+
 }
 
 function választVálasz(i, btnRef) {
     let jóVálasz = kérdések[jelenlegiKérdés].correctAnswer;
-    if (jóValasz == i) {
-        btnRef.classList.add("jo");
+    if (jóVálasz == i) {
+        btnRef.classList.add("jó");
     }
     else {
         btnRef.classList.add("rossz")
